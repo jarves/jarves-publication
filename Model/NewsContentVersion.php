d985cba5573ac2952528e3ef6bcbcd929ee24426
Marc J. Schmidt <marc@kryn.org>
2014-07-29 20:50:32 +0200

    Fixed contentInterface

---
+++ Model/NewsContentVersion.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Jarves\Publication\Model;
+
+use Jarves\Publication\Model\Base\NewsContentVersion as BaseNewsContentVersion;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'jarves_publication_news_content_version' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class NewsContentVersion extends BaseNewsContentVersion
+{
+
+}
---
+++ Model/NewsIntro.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Jarves\Publication\Model;
+
+use Jarves\Model\ContentInterface;
+use Jarves\Publication\Model\Base\NewsIntro as BaseNewsIntro;
+
+
+/**
+ * Skeleton subclass for representing a row from the 'jarves_publication_news_intro' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class NewsIntro extends BaseNewsIntro implements ContentInterface
+{
+
+}
